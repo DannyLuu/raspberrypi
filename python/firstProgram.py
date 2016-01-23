501b2b883af1d176a629238147d035b120afcb42
Danny <hisoka365@gmail.com>
2016-01-23 06:48:28 +0000

    First Python program

---
+++ python/firstProgram.py
@@ -0,0 +1,47 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led=17
+
+GPIO.setup(led,GPIO.OUT)
+
+print("Light On")
+GPIO.output(led,GPIO.HIGH)
+
+time.sleep(1)
+print("Light Off")
+GPIO.output(led,GPIO.LOW)
+
+time.sleep(1)
+print("Light On")
+GPIO.output(led,GPIO.HIGH)
+
+time.sleep(1)
+print("Light Off")
+GPIO.output(led,GPIO.LOW)
+time.sleep(1)
+print("Light On")
+GPIO.output(led,GPIO.HIGH)
+
+time.sleep(1)
+print("Light Off")
+GPIO.output(led,GPIO.LOW)
+time.sleep(1)
+print("Light On")
+GPIO.output(led,GPIO.HIGH)
+
+time.sleep(1)
+print("Light Off")
+GPIO.output(led,GPIO.LOW)
+time.sleep(1)
+print("Light On")
+GPIO.output(led,GPIO.HIGH)
+
+time.sleep(1)
+print("Light Off")
+GPIO.output(led,GPIO.LOW)
+
+GPIO.cleanup()
